2aad2c90b574029230b20ed9ed44dad46511d626
Edward5051 <edante12345678@gmail.com>
2023-12-14 20:55:50 +0100

    deprecated the use of app.js, all browser routes now moved to index.js file, more optimised

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import SignupPage from './pages/signup';
+import SigninPage from './pages/signin';
+import DashboardPage from './pages/dashboard';
+import ProductPage from './pages/productlistings';
+
 import './index.css';
-import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<SignupPage/>} />
+          <Route path='/signin' element={<SigninPage/>} />
+          <Route path='/dashboard' element={<DashboardPage/>} />
+          <Route path='/productpage' element={<ProductPage/>} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+    
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
